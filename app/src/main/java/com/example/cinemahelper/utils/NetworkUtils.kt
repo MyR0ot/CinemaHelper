1bb7b800dfa2f0c1fd59911e831ad1de0249bdcb
Ivan Pavlov <31338991+MyR0ot@users.noreply.github.com>
2019-11-06 17:12:44 +0300

    async load content

---
+++ app/src/main/java/com/example/cinemahelper/utils/NetworkUtils.kt
@@ -0,0 +1,52 @@
+package com.example.cinemahelper.utils
+
+import android.net.Uri
+import java.io.IOException
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.URL
+import java.util.*
+
+object NetworkUtils {
+
+    private const val VK_API_BASE_URL = "https://api.vk.com"
+    private const val VK_USER_GET = "/method/users.get"
+    private const val PARAM_USER_ID = "user_ids"
+    private const val PARAM_VERSION = "v"
+    private const val PARAM_ACCESS_TOKEN = "access_token"
+    private const val ACCESS_TOKEN = "a4021ebfa4021ebfa4021ebfa6a46fcbf8aa402a4021ebff9c0af8458d7dc95541b0952"
+
+    public fun generateURL(userId: String): URL? {
+        val buildUri: Uri = Uri.parse(VK_API_BASE_URL + VK_USER_GET)
+            .buildUpon()
+            .appendQueryParameter(PARAM_ACCESS_TOKEN, ACCESS_TOKEN)
+            .appendQueryParameter(PARAM_USER_ID, userId)
+            .appendQueryParameter(PARAM_VERSION, "5.8")
+            .build()
+
+        return URL(buildUri.toString())
+    }
+
+
+    @Throws(IOException::class)
+    public fun getResponseFromURL(url: URL): String?{
+        val urlConnection: HttpURLConnection = url.openConnection() as HttpURLConnection
+
+        try {
+            println("test 1")
+            val inStream: InputStream = urlConnection.inputStream
+            val scanner: Scanner = Scanner(inStream)
+            scanner.useDelimiter("\\A")
+
+            val hasInput: Boolean = scanner.hasNext()
+
+            return if(hasInput) {
+                scanner.next();
+            } else {
+                null;
+            }
+        } finally {
+            urlConnection.disconnect()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/cinemahelper/utils/ParserUtil.kt
@@ -0,0 +1,126 @@
+package com.example.cinemahelper.utils
+
+import android.os.StrictMode
+import com.example.cinemahelper.Film
+import org.apache.http.HttpResponse
+import org.apache.http.client.HttpClient
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.impl.client.DefaultHttpClient
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.lang.Exception
+import java.lang.StringBuilder
+
+object ParserUtil {
+
+    private val parseMap: Map<String, Pair<String, String>> = mapOf(
+        "name" to Pair("<h1 class=\"u_mb-xl\">", "</h1>"),
+        "duration" to Pair("<nobr class=\"u_mr-m\">", "</nobr>"),
+        "description" to Pair("itemProp=\"description\">", "</truncate-text>"),
+        "genres" to Pair("<meta itemprop=\"genre\" content=\"", "\">"),
+        "img" to Pair("<meta itemProp=\"image\" content=\"", "\"/>"),
+        "tags" to Pair("<span class=\"film_label\">", "</span>")
+    )
+
+
+    fun loadContent():HashSet<Film> {
+        val films: HashSet<Film> = HashSet();
+        val mainPageHTML = loadHTML("https://cinemadelux.ru/", "utf-8")
+        val ids: HashSet<String>? = mainPageHTML?.let { parseIds(it) }
+        for(id in ids!!){
+            val film: Film? = parseFilm("https://cinemadelux.ru/films/$id", id)
+            film?.let { films.add(it) }
+        }
+
+        return films
+    }
+
+    /*
+    private fun loadImage(url: String, imgView: ImageView): Unit {
+        Glide.with(this) // TODO: найти нужный контекст (вместо this)
+            .load(url).into(imgView)
+    }
+    */
+
+
+    private fun loadHTML(url: String, charsetName: String = "windows-1251"): String?  {
+        val sb: StringBuilder = StringBuilder()
+        val policy: StrictMode.ThreadPolicy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
+        val httpClient: HttpClient = DefaultHttpClient()
+        try {
+            val response: HttpResponse = httpClient.execute(HttpGet(url))
+            val inputStream: InputStream = response.entity.content
+            val reader: BufferedReader = BufferedReader(InputStreamReader(inputStream, charsetName), 8)
+            do {
+                val line = reader.readLine() ?: break
+                sb.append(line + "\n")
+            } while (true)
+            return sb.toString()
+        }catch(e: Exception)
+        {
+            e.printStackTrace()
+        }
+
+        return null
+    }
+
+    private fun parseIds(html: String, startPattern: String = "<a href=\"/films/"): HashSet<String> {
+        val res: HashSet<String> = HashSet<String>()
+        var beginIndex: Int = html.indexOf(startPattern)
+        val limitIndex = html.indexOf("Скоро в кино") // не учитываем фильмы в разделе 'Скоро в кино'
+        while(beginIndex != -1 && beginIndex < limitIndex) {
+            var length = 0;
+            while(html[beginIndex + length + startPattern.length].isDigit()){
+                length++
+            }
+            try {
+                res.add(html.substring(beginIndex + startPattern.length, beginIndex + startPattern.length + length))
+            } catch (e: Exception){
+            }
+            beginIndex = html.indexOf(startPattern, beginIndex + startPattern.length)
+        }
+        return res
+    }
+
+    private fun parseOneString(text: String, startWord: String, stopWord: String): String {
+        val beginIndex: Int = text.indexOf(startWord) + startWord.length
+        return text.substring(beginIndex, text.indexOf(stopWord, beginIndex))
+    }
+
+    private fun parseAllStrings(text: String, startWord: String, stopWord: String): List<String> {
+        val res: MutableList<String> = mutableListOf()
+        var beginIndex: Int = text.indexOf(startWord)
+
+        while(beginIndex != -1){
+            beginIndex += startWord.length
+            val endIndex = text.indexOf(stopWord, beginIndex)
+            if(endIndex == -1) break
+            res.add(text.substring(beginIndex, endIndex))
+            beginIndex = text.indexOf(startWord, beginIndex)
+        }
+
+        return res.toList()
+    }
+
+    private fun parseFilm(url: String, id: String): Film? {
+        val html: String? = loadHTML(url, "utf-8")
+        html ?: return null
+
+        try {
+            val name: String = parseOneString(html,  parseMap["name"]!!.first,  parseMap["name"]!!.second)
+            val duration: String = parseOneString(html,  parseMap["duration"]!!.first,  parseMap["duration"]!!.second)
+            val description: String = parseOneString(html,  parseMap["description"]!!.first,  parseMap["description"]!!.second)
+            val img: String = parseOneString(html,  parseMap["img"]!!.first,  parseMap["img"]!!.second)
+            val tags: List<String> = parseAllStrings(html, parseMap["tags"]!!.first, parseMap["tags"]!!.second)
+            val genres: List<String> = parseOneString(html,  parseMap["genres"]!!.first, parseMap["genres"]!!.second).split(", ") // TODO: Проверить
+
+            return Film(id, name, description, duration, genres, tags, img)
+        } catch (e: Exception){
+            System.err.println("Parse error!  filmID: $id")
+            return null
+        }
+    }
+
+}
