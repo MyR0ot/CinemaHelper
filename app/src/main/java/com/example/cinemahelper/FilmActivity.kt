84c1c3b176f8da5b47f1c6d693d1d0a2e79b2c19
Ivan Pavlov <31338991+MyR0ot@users.noreply.github.com>
2019-11-10 01:33:01 +0300

    swipe gestur

--- app/src/main/java/com/example/cinemahelper/FilmActivity.kt
+++ app/src/main/java/com/example/cinemahelper/FilmActivity.kt
@@ -11,9 +11,91 @@ import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_film.*
 import android.content.Intent
 import android.net.Uri
+import android.view.GestureDetector
+import android.view.MotionEvent
+import androidx.core.view.GestureDetectorCompat
+import android.widget.Toast
 
 
-class FilmActivity : AppCompatActivity() {
+
+
+class FilmActivity : GestureDetector.OnGestureListener, AppCompatActivity() {
+    override fun onScroll(
+        e1: MotionEvent?,
+        e2: MotionEvent?,
+        distanceX: Float,
+        distanceY: Float
+    ): Boolean {
+        return true
+    }
+
+    private val SWIPE_THRESHOLD = 100
+    private val SWIPE_VELOCITY_THRESHOLD = 100
+
+    private lateinit var  gd: GestureDetectorCompat
+
+    fun onSwipeTop() {
+    }
+
+    fun onSwipeRight() {
+        finish()
+    }
+
+    fun onSwipeLeft() {
+    }
+
+    fun onSwipeBottom() {
+    }
+
+    override fun onSingleTapUp(e: MotionEvent?): Boolean {
+        return true
+    }
+
+    override fun onDown(e: MotionEvent?): Boolean {
+        return true
+    }
+
+    override fun onFling(e1: MotionEvent?, e2: MotionEvent?, velocityX: Float, velocityY: Float): Boolean {
+        var result = false
+        try {
+            val diffY = e2!!.getY() - e1!!.getY()
+            val diffX = e2!!.getX() - e1!!.getX()
+            if (Math.abs(diffX) > Math.abs(diffY)) {
+                if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                    if (diffX > 0) {
+                        onSwipeRight()
+                    } else {
+                        onSwipeLeft()
+                    }
+                    result = true
+                }
+            } else if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                if (diffY > 0) {
+                    onSwipeBottom()
+                } else {
+                    onSwipeTop()
+                }
+                result = true
+            }
+        } catch (exception: Exception) {
+            exception.printStackTrace()
+        }
+
+        return result
+    }
+
+
+    override fun onLongPress(e: MotionEvent?) {
+    }
+
+    override fun onShowPress(e: MotionEvent?) {
+    }
+
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        this.gd.onTouchEvent(event)
+        return super.onTouchEvent(event)
+    }
 
     private lateinit var film: Film
 
@@ -21,8 +103,7 @@ class FilmActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_film)
         getInfoFromIntent()
-
-
+        gd = GestureDetectorCompat(this, this)
         inflate()
     }
 
